3b3f0e85763eb91545bfdc6dc94d19ace282b416
Lucas Albert <158320153+lucasxalbert@users.noreply.github.com>
2024-04-05 11:12:06 -0300

    Upload 1001-1005

---
+++ 1004/1004.py
@@ -0,0 +1,4 @@
+PROD = int(input())
+PROD1 = int(input())
+PROD2 = PROD * PROD1
+print("PROD = %s" %PROD2)
---
+++ 1005/1005.py
@@ -0,0 +1,6 @@
+A = float(input())
+B = float(input())
+MEDIA1 = A * 3.5
+MEDIA2 = B * 7.5
+MEDIA3 = (MEDIA1 + MEDIA2) /11
+print("MEDIA = %.5f" %MEDIA3)
